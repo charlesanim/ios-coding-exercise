e3ab14340f673ce5dea5dc802981a232b406e680
Charles Anim <charles.anim@wellinks.com>
2022-01-31 21:19:18 -0500

    Initial Commit

---
+++ CodingExercise/CodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingExerciseApp.swift
+//  CodingExercise
+//
+//  Created by Charles Anim on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodingExercise
+//
+//  Created by Charles Anim on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
